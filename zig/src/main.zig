faf121c5033a8bcc5597c131837d0992a2867d5d
Jakob Bergman <jakob@aino.se>
2021-12-23 22:54:40 +0100

    Try using structs in Zig

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -1,7 +1,20 @@
 const std = @import("std");
 
+const Set = struct {
+    A: []const u8,
+    B: []const u8,
+    C: []const u8,
+};
+
+var cyberpunk = Set{
+    .A = "ΔΛА▲4",
+    .B = "8",
+    .C = "©",
+};
+
 pub fn main() anyerror!void {
     std.log.info("The Zig version of karakter is a work in progress.", .{});
+    std.log.info("{s}\n", .{cyberpunk.A});
 }
 
 test "basic test" {
