0f09c069c3f504fba31551bb0db92a726966e0f8
Jakob Bergman <jakob@aino.se>
2021-12-24 12:07:54 +0100

    Zig: Try using structs for sets

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -1,22 +1,17 @@
 const std = @import("std");
 
-const Set = struct {
-    A: []const u8,
-    B: []const u8,
-    C: []const u8,
-};
-
-var cyberpunk = Set{
-    .A = "ΔΛА▲4",
-    .B = "8",
-    .C = "©",
-};
+const cyberpunk = @import("sets/cyberpunk.zig");
+const fairytale = @import("sets/fairytale.zig");
+const standard = @import("sets/standard.zig");
 
 pub fn main() anyerror!void {
-    std.log.info("The Zig version of karakter is a work in progress.", .{});
-    std.log.info("{s}\n", .{cyberpunk.A});
+  std.log.info("The Zig version of karakter is a work in progress.", .{});
+  const A = @field(cyberpunk.set, "A");
+  const B = @field(fairytale.set, "B");
+  const C = @field(standard.set, "C");
+  std.log.info("Examples:\n{s}\n{s}\n{s}\n", .{A, B, C});
 }
 
 test "basic test" {
-    try std.testing.expectEqual(10, 3 + 7);
+  try std.testing.expectEqual(10, 3 + 7);
 }
